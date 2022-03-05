a4fa856db585fa100fcb51e0f6dd28bae7b62d23
Bronmal <tri2004@yandex.ru>
2022-03-05 17:01:33 +0300

    first commit

---
+++ VIN_LIB/VIN_LIB.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIN_LIB
+{
+    public class VIN_LIB
+    {
+        public Boolean CheckVIN(String vin)
+        {
+            Dictionary<char, int> dict_char_and_int = new Dictionary<char, int>()
+            {
+                {'A', 1 },
+                {'B', 2 },
+                {'C', 3 },
+                {'D', 4 },
+                {'E', 5 },
+                {'F', 6 },
+                {'G', 7 },
+                {'H', 8 },
+                {'J', 1 },
+                {'K', 2 },
+                {'L', 3 },
+                {'M', 4 },
+                {'N', 5 },
+                {'P', 7 },
+                {'R', 9 },
+                {'S', 2 },
+                {'T', 3 },
+                {'U', 4 },
+                {'V', 5 },
+                {'W', 6 },
+                {'X', 7 },
+                {'Y', 8 },
+                {'Z', 9 }
+            };
+
+            int[] weight = new int[17]
+            {
+                8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2
+            };
+
+            if (vin.Length != 17)
+            {
+                return false;
+            }
+
+            char[] char_numbers_vin = vin.ToArray();
+            foreach (char c in char_numbers_vin)
+            {
+                if (c == 'I' || c == 'O' || c == 'Q')
+                {
+                    return false;
+                }
+            }
+
+            int[] changed_letter_to_num = new int[17];
+            for(int c = 0; c < char_numbers_vin.Length; c++)
+            {
+                int temp;
+                if (!int.TryParse(char_numbers_vin[c].ToString(), out temp))
+                {
+                    changed_letter_to_num[c] = dict_char_and_int[char_numbers_vin[c]];
+                }
+                else { changed_letter_to_num[c] = temp; }
+                
+            }
+            double check_sum = 0;
+
+            for (int i = 0; i < changed_letter_to_num.Length; i++)
+            {
+                if (i == 8) { continue; }
+                check_sum = check_sum + (changed_letter_to_num[i] * weight[i]);
+            }
+            if (check_sum - (Math.Floor(check_sum * 11) * 11) == 10)
+            {
+                if (char_numbers_vin[8] != 'X')
+                {
+                    return false;
+                }
+            }
+            if (check_sum - (Math.Floor(check_sum / 11) * 11) != changed_letter_to_num[8])
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public String GetVINCountry(String vin)
+        {
+            return vin;
+        }
+
+        public int GetTransportYear(String vin)
+        {
+            return 1;
+        }
+    }
+}
---
+++ WSR/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VIN_LIB;
+
+namespace WSR
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            VIN_LIB.VIN_LIB vIN_LIB = new VIN_LIB.VIN_LIB();
+            MessageBox.Show(vIN_LIB.CheckVIN(enter_vin.Text).ToString());
+        }
+    }
+}
