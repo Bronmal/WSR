c0e73c6d1e584cf99cc0c3e60c8a37551d0cd014
Bronmal <tri2004@yandex.ru>
2022-03-07 13:18:17 +0300

    lsuefhsef

---
+++ REG_MARK_LIB/REG_MARK_LIB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REG_MARK_LIB
+{
+    public class REG_MARK_LIB
+    {
+        public Boolean CheckMark(string mark)
+        {
+            char[] alpabet = new char[12] { 'A', 'B', 'E', 'K', 'M', 'H', 'O', 'P', 'C', 'T', 'Y', 'X' };
+            char[] char_mark_array = mark.ToCharArray();
+            char[] check_letters = new char[3] { char_mark_array[0], char_mark_array[4], char_mark_array[5] };
+            foreach (char i in check_letters)
+            {
+                if (!alpabet.Contains(i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
